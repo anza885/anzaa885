7290beb2620daa7f18fc14eda793a90aa92e6a5a
anza885 <anzalnashah5572@gmail.com>
2025-04-24 07:26:06 -0700

    Add files via upload

---
+++ Mid Term/script.js
@@ -0,0 +1,19 @@
+function calculate(num1, num2, operator) {
+    if (operator === '+') {
+      return num1 + num2;
+    } else if (operator === '*') {
+      return num1 * num2;
+    }
+     else {
+      return "Invalid operator";
+    }
+  }
+
+
+
+
+  
+
+
+    document.body.innerHTML=(5,2, "+");
+    document.body.innerHTML=(5,1, "*");
